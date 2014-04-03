b39ea847546887bfbba1c026021993f4ec76efdc
James Rowen <jamesrowen@gmail.com>
2014-04-03 01:03:31 -0500

    working on settings tab and matchMedia
    settings tab
    - made it minimize at a larger size
    - fixed the border color at smaller widths
    changed checkMobile to mediaQueries
    - added a mid-size breakpoint
    - switch to use the matchMedia feature
    - added paulIrish's matchMedia polyfill for legacy brower support

--- js/main.js
+++ js/main.js
@@ -24,29 +24,29 @@ var scope = {
 	});
 
 	// jump to links
-	$('#home-link').click(function() { 
+	$('#home-link').click(function() {
 		$('html, body').animate({ scrollTop:$('#home').offset().top }, 300, function(){});
 	});
-	$('#resume-link').click(function() { 
+	$('#resume-link').click(function() {
 		$('html, body').animate({ scrollTop:$('#resume').offset().top }, 300, function(){});
 	});
 
 	// toggle settings menu
-	$('#settings-tab').click(function() { 
-		$(this).parent().animate({ 
-			right:$(this).hasClass('active') ? scope.settingsRight + 'px' : '0' 
+	$('#settings-tab').click(function() {
+		$(this).parent().animate({
+			right:$(this).hasClass('active') ? scope.settingsRight + 'px' : '0'
 		}, 300, function(){});
 		$(this).toggleClass('active');
 	});
 
 	// toggle light/dark color scheme
-	$('#dark').click(function() { 
+	$('#dark').click(function() {
 		$('#dark').addClass('active');
 		$('#light').removeClass('active');
 		$('#page').animate({ backgroundColor:'#333', color:'#ddd' }, 500, function(){});
 		$('#scrollbar').animate({ borderColor:'#333', backgroundColor:'#ddd' }, 500, function(){});
 	});
-	$('#light').click(function() { 
+	$('#light').click(function() {
 		$('#light').addClass('active');
 		$('#dark').removeClass('active');
 		$('#page').animate({ backgroundColor:'#e0e0e0', color:'#484848' }, 500, function(){});
@@ -54,24 +54,24 @@ var scope = {
 	});
 
 	// toggle lowercase/uppercase headers
-	$('#lower').click(function() { 
+	$('#lower').click(function() {
 		$('#lower').addClass('active');
 		$('#upper').removeClass('active');
 		$('#nav,#settings,h2,h3').css('text-transform', 'lowercase');
 	});
-	$('#upper').click(function() { 
+	$('#upper').click(function() {
 		$('#upper').addClass('active');
 		$('#lower').removeClass('active');
 		$('#nav,#settings,h2,h3').css('text-transform', 'uppercase');
 	});
 
 	// toggle normal/bold links on left side
-	$('#weight-normal').click(function() { 
+	$('#weight-normal').click(function() {
 		$('#weight-normal').addClass('active');
 		$('#weight-bold').removeClass('active');
 		$('#nav,#settings').css('font-weight', 'normal');
 	});
-	$('#weight-bold').click(function() { 
+	$('#weight-bold').click(function() {
 		$('#weight-bold').addClass('active');
 		$('#weight-normal').removeClass('active');
 		$('#nav,#settings').css('font-weight', 'bold');
@@ -92,7 +92,7 @@ var scope = {
 	// left nav link hover
 	var linkAnimateTime = 150;
 	$('#nav a').hover(
-		function() { 
+		function() {
 			var id = $(this).attr('id');
 			var newColor = $('a.active').css('color');
 			if (id == "email")
@@ -102,8 +102,8 @@ var scope = {
 			else if (id == "facebook")
 				newColor = "#44619D";
 			$(this).parent().animate({
-				color: newColor, "margin-left": "5px", borderBottomColor: newColor}, { duration: linkAnimateTime }); 
-		}, 
+				color: newColor, "margin-left": "5px", borderBottomColor: newColor}, { duration: linkAnimateTime });
+		},
 		function() { $(this).parent().animate({
 			color: "#888", "margin-left": "0", borderBottomColor: "#888"}, { duration: linkAnimateTime }); }
 	);
@@ -117,7 +117,7 @@ var scope = {
 		clickY = e.screenY;
 
 		$(document).bind("mousemove.scrolling", function(e){
-			// calculate the new scroll percentage, based on the current mouse position relative to its 
+			// calculate the new scroll percentage, based on the current mouse position relative to its
 			// position at the beginning of the drag. constrain the scrollbar to the min/max positions
 			var dragDelta = e.screenY - clickY;
 			var newScrollPos = Math.min(Math.max(clickScrollPos + dragDelta, minScrollPos), maxScrollPos);
@@ -151,35 +151,43 @@ function onResize(e)
 		$(document).trigger('scroll');
 		maxScrollPos = $(window).height() - 40 - 25;
 
-		checkMobile();
+		mediaQueries();
 }
 
-// during a resize event, check if we are at mobile size
-function checkMobile() 
+// during a resize event, perform js media queries
+function mediaQueries()
 {
-	var breakpoint = 632;
-	// mobile
-	if ($(window).width() <= breakpoint)
-	{
-		// add click handler to toggle the menu
-		$("#nav").unbind("click").bind("click", toggleNav);
+	// mobile - default
 
-		$('#settings-tab').html('<a>&lt;</a>').width('10px').css({ 'left': '-24px', 'padding':'3px 6px 3px 6px', 'background-color': 'inherit' });
-		scope.settingsRight = -141;
+	// left menu - click on it to open/close
+	$("#nav").unbind("click").bind("click", toggleNav);
+
+	// settings menu - anchor shows small arrow icon
+	$('#settings-tab').html('<a>&lt;</a>').css({
+		'width': '10px',
+		'left': '-24px',
+		'padding':'3px 6px 3px 6px',
+		'background-color': 'inherit'
+	});
+	scope.settingsRight = -141;
+
+	// middle size
+	if (matchMedia("(min-width: 40.5em)").matches)
+	{
+		// left menu - remove click event and styles it may have applied
+		$("#nav").unbind("click").removeClass('active').removeAttr('style');
 	}
 
-	// non-mobile
-	else
+	// full size
+	if (matchMedia("(min-width: 65em)").matches)
 	{
-		// clear the click event and any styles it applied
-		$("#nav").unbind("click").removeClass('active').removeAttr('style');
-
+		// settings menu - anchor shows full "settings" text
 		$('#settings-tab').html('<a>settings</a>').width('80px').removeAttr('style');
 		scope.settingsRight = -135;
 	}
 };
 
-function onScroll(e) 
+function onScroll(e)
 {
 	for (var i=0; i<document.styleSheets.length;i++) {}
 	var top = $(document).scrollTop();
@@ -198,20 +206,20 @@ function toggleNav()
 	$("#nav")
 		.animate({ left: isNavOpen ? -distInEM + 'em' : '0' }, duration, function(){})
 		.toggleClass('active')
-		// .parent().animate({ 
-		// 	marginLeft: isNavOpen ? -dx + 'px' : '0', 
-		// 	marginRight: isNavOpen ? '0' : -dx + 'px' 
+		// .parent().animate({
+		// 	marginLeft: isNavOpen ? -dx + 'px' : '0',
+		// 	marginRight: isNavOpen ? '0' : -dx + 'px'
 		// }, duration, function(){});
 }
 
-function setFont(e) 
+function setFont(e)
 {
 		$('.set-font a').removeClass('active');
 		$(e.data.divId).addClass('active');
 		$('#page').css('font-family', e.data.font);
 };
 
-function setLinkColor(e) 
+function setLinkColor(e)
 {
 		$('.set-link-color a').removeClass('active');
 		$(e.data.divId).addClass('active');
