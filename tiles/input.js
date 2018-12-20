2e6a8c79a2b4bfd661b42e97ac3c359d21306dd6
James Rowen <jamesrowen@gmail.com>
2018-12-20 09:27:11 -0800

    update to new site

---
+++ tiles/input.js
@@ -0,0 +1,200 @@
+let lastDragX, lastDragY, dragged = false;
+
+// keyboard handler
+window.addEventListener('keydown', e => {
+  switch(e.keyCode) {
+    case 27: // esc
+      toggleControls();
+      break;
+    case 32: // space
+      if (guidePos == 1) {
+        nextGuideStep(2000);
+      }
+      updateSetting('playing', !playing);
+      e.preventDefault();
+      break;
+    case 188: // comma (<)
+      setStep(-1);
+      break;
+    case 190: // period (>)
+      setStep(1);
+      break;
+    case 82: // R
+      if (guidePos == 2) {
+        nextGuideStep(3000);
+      }
+      if (!e.ctrlKey && !e.metaKey)
+        updateSetting('rewind', !rewind);
+      break;
+    case 187: // equal/plus
+      mode == 'spin' ? updateSetting('tileSize', min(tileSize + 1, 180)) :
+        updateZoom(min(zoom + 1, 120));
+      break;
+    case 189: // minus
+      mode == 'spin' ? updateSetting('tileSize', max(tileSize - 1, 40)) :
+        updateZoom(max(zoom - 1, 4));
+      break;
+  }
+});
+
+
+// mouse handler
+function setMouseListeners(element) {
+  element.addEventListener('wheel', e => {
+    if (mode == 'spin' && playing == false) {
+      updateSetting('spinY', spinY - e.deltaX / 500);
+      updateSetting('spinX', spinX - e.deltaY / 500);
+    }
+    if (mode == 'orbit') {
+      panCam(e.deltaX, e.deltaY);
+    }
+    e.preventDefault();
+  });
+
+  element.onmousedown = e => {
+    dragged = false;
+    if (mode == 'orbit') {
+      lastDragX = e.clientX;
+      lastDragY = e.clientY;
+      document.onmousemove = e => {
+        dragged = true;
+        panCam(lastDragX - e.clientX, lastDragY - e.clientY);
+        lastDragX = e.clientX;
+        lastDragY = e.clientY;
+      };
+      document.onmouseup = e => document.onmousemove = null;
+    }
+  };
+
+  element.onmouseup = e => {
+    if (!dragged)
+      toggleControls();
+  };
+}
+
+
+// general actions
+function setMode(mode) {
+  if (guidePos == 5 && mode == 'pattern')
+    nextGuideStep(1500);
+  if (mode == 'orbit' && orbitHelpPos == -1) {
+    updateSetting('rewind', true);
+    window.setTimeout(() => {
+      if (this.mode == 'orbit') {
+        updateSetting('orbitHelpPos', 0);
+        el('orbitBubble').classList.add('open');
+      }
+    }, 10000);
+  }
+
+  updateSetting('mode', mode);
+}
+
+function setStep(val) {
+  step = val;
+}
+
+function resetPos() {
+  updateSetting(mode, 0);
+  if (mode == 'spin') {
+    updateSetting('spinX', 0);
+    updateSetting('spinY', 0);
+  }
+}
+
+function useModeSlider(id, value) {
+  if (guidePos == 0) {
+    nextGuideStep(3000);
+  }
+  updateSetting(id, value);
+}
+
+function toggleControls() {
+  if (guidePos == 9) {
+    nextGuideStep(4000);
+  }
+  el('mode').classList.toggle('closed');
+}
+
+function clickPreset(name) {
+  if (guidePos == 3 || guidePos == 8)
+    nextGuideStep(3500);
+  loadPreset(name);
+}
+
+function clickColorPreset(name) {
+  if (guidePos == 4)
+    nextGuideStep(4000);
+  loadPreset(name);
+}
+
+// spin mode actions
+function clearSpinX() {
+  updateSetting('spinX', 0);
+  updateSetting('spinXSpeed', 0);
+}
+
+function clearSpinY() {
+  updateSetting('spinY', 0);
+  updateSetting('spinYSpeed', 0);
+}
+
+
+// pattern mode actions
+function toggleLoop() {
+  if (guidePos == 7)
+    nextGuideStep(4000);
+
+  updateSetting('loopTrans', !loopTrans);
+}
+
+function addTransition(transition, time) {
+  if (guidePos == 6)
+    nextGuideStep(2000);
+
+  if (patternList.length == 0) {
+    patternList = shuffle(Object.keys(patterns));
+  }
+  let newPattern = patternList.pop();
+
+  // by default start at the beginning or end (ok if time == 0)
+  if (!time)
+    time = rewind ? 1 : 0;
+  // wrap around if coming from a previous transition
+  if (time < 0)
+    time += 1;
+  if (time > 1)
+    time -= 1;
+
+  if (rewind) {
+    let lastPattern = curPattern;
+    updateSetting('curPattern', newPattern);
+    newPattern = lastPattern;
+    curTransitions.unshift([newPattern, transition, time]);
+  } else {
+    curTransitions.push([newPattern, transition, time]);
+  }
+}
+
+
+// orbit mode actions
+function updateZoom(val) {
+  updateSetting('camX', camX + (camX - width / 2) / zoom * (val - zoom));
+  updateSetting('camY', camY + (camY - height / 2) / zoom * (val - zoom));
+  updateSetting('zoom', val);
+}
+
+function resetCamPos() {
+  updateSetting('camX', 0);
+  updateSetting('camY', 0);
+}
+
+function panCam(dx, dy) {
+  updateSetting('camX', camX - dx);
+  updateSetting('camY', camY - dy);
+
+  if (orbitHelpPos == 0) {
+    updateSetting('orbitHelpPos', 1);
+    el('orbitBubble').classList.remove('open');
+  }
+}
---
+++ tiles/settings.js
@@ -0,0 +1,145 @@
+var el = document.getElementById.bind(document);
+function elements(selector) {
+  return Array.prototype.slice.call(document.querySelectorAll(selector));
+}
+
+var settings = {
+  'mode': {default: 'spin', parse: x => x},
+  // playback
+  'playing': {default: true, parse: parseBool},
+  'rewind': {default: false, parse: parseBool},
+  'spinSpeed': {default: 15, parse: parseInt, type: 'slider', tween: true},
+  'patternSpeed': {default: 30, parse: parseInt, type: 'slider', tween: true},
+  'orbitSpeed': {default: 40, parse: parseInt, type: 'slider', tween: true},
+  // colors
+  'color1a': {default: '#101010', parse: x => x, tween: true},
+  'color2a': {default: '#f0f0f0', parse: x => x, tween: true},
+  'color1b': {default: '#101010', parse: x => x, tween: true},
+  'color2b': {default: '#f0f0f0', parse: x => x, tween: true},
+  'c1mode': {default: 0, parse: parseInt},
+  'c2mode': {default: 0, parse: parseInt},
+  'c1alpha': {default: 1, parse: parseFloat, type: 'slider', tween: true},
+  'c2alpha': {default: 1, parse: parseFloat, type: 'slider', tween: true},
+  'bgColor': {default: '#5b5b5b', parse: x => x, tween: true},
+  // spin mode
+  'spin': {default: 0, parse: parseFloat, type: 'input', tween: true},
+  'spinY': {default: 0, parse: parseFloat, tween: true},
+  'spinYSpeed': {default: 0, parse: parseInt, type: 'slider', tween: true},
+  'spinX': {default: 0, parse: parseFloat, tween: true},
+  'spinXSpeed': {default: 0, parse: parseInt, type: 'slider', tween: true},
+  'tileSize': {default: 63, parse: parseInt, type: 'slider', tween: true},
+  'tileScale': {default: .96, parse: parseFloat, type: 'slider', tween: true},
+  'margin': {default: 32, parse: parseInt, type: 'slider', tween: true},
+  'shapeIterations': {default: 8, parse: parseInt, type: 'slider', tween: true},
+  // patternMode
+  'pattern': {default: 0, parse: parseFloat, type: 'input', tween: true},
+  'curPattern': {default: 'herringbone', parse: x => x},
+  'loopTrans': {default: false, parse: parseBool},
+  'curTransitions': {default: [], parse: JSON.parse},
+  'patTileSize': {default: 56, parse: parseInt, type: 'slider', tween: true},
+  'patTileScale': {default: 1, parse: parseFloat, type: 'slider', tween: true},
+  'transLength': {default: .3, parse: parseFloat, type: 'slider', tween: true},
+  'shortRotations': {default: false, parse: parseBool},
+  'rotX': {default: false, parse: parseBool},
+  'rotY': {default: false, parse: parseBool},
+  // orbit mode
+  'orbit': {default: 4.19, parse: parseFloat, type: 'input', tween: true},
+  'curl': {default: 22, parse: parseInt, type: 'slider', tween: true},
+  'numTiles': {default: 270, parse: parseInt, type: 'slider', tween: true},
+  'zoom': {default: 63, parse: parseInt, type: 'slider', tween: true},
+  'growth': {default: 100, parse: parseInt, type: 'slider', tween: true},
+  'camX': {default: 22, parse: parseFloat, tween: true},
+  'camY': {default: 22, parse: parseFloat, tween: true},
+  // help text
+  'firstTime': {default: true, parse: parseBool},
+  'guidePos': {default: -1, parse: parseInt},
+  'orbitHelpPos': {default: -1, parse: parseInt}
+};
+
+function parseBool(val) {
+  return val == 'false' ? false : (val == 'true' ? true : !!val);
+}
+
+function updateSetting(name, value) {
+  if (settings[name].parse == JSON.parse) {
+    this[name] = value;
+    window.localStorage.setItem(name, JSON.stringify(value));
+    return;
+  }
+  let val = settings[name].parse(value);
+  let str = val;
+
+  this[name] = val;
+  window.localStorage.setItem(name, val);
+
+  if (name == 'spin' || name == 'pattern' || name == 'orbit') {
+    val = val.toFixed(1);
+  }
+
+  if (el(name)) {
+    el(name).setAttribute('value', val);
+    el(name).value = val;
+  }
+
+  if (settings[name].parse == parseFloat)
+    str = str.toFixed(1);
+  str = "" + str;
+  if (str[0] == '0' && str[1] == '.')
+    str = str.slice(1);
+
+  if (settings[name].type == 'slider') {
+    el(name).parentNode.querySelector('span').textContent = str;
+  }
+}
+
+let guideSteps = [
+  ['-450px', 'Play with some sliders', '#modes .slider'],
+  ['-836px', 'Press Space to pause', 'none'],
+  ['-746px', 'Press R to rewind', 'none'],
+  ['-115px', 'Try the presets', '#presets .button'],
+  [  '78px', 'And the color presets', '#colorPresets .button'],
+  ['-469px', 'Click here to change mode', '#modeTabs div:nth-child(2)'],
+  ['-315px', 'Click some transitions', '.trans'],
+  ['-444px', 'Click here to keep them looping', '#loopTrans'],
+  ['-140px', 'Each mode has presets', '#presets .button'],
+  ['-450px', 'Press Esc or click the canvas to hide the controls', 'none']
+];
+function nextGuideStep(delay) {
+  el('bubble').classList.remove('open');
+  elements(guideSteps[guidePos][2]).map(e => e.classList.remove('highlight'));
+  updateSetting('guidePos', guidePos + 1);
+  window.setTimeout(setGuide, delay);
+}
+function setGuide() {
+  if (guidePos < 10) {
+    let bubble = el('bubble');
+    bubble.style.left = guideSteps[guidePos][0];
+    el('bubbleText').textContent = guideSteps[guidePos][1];
+    bubble.classList.add('open');
+
+    elements(guideSteps[guidePos][2]).map(e => e.classList.add('highlight'));
+    if (guidePos == 2)
+      updateSetting('playing', true);
+    if (guidePos == 6)
+      updateSetting('rewind', false);
+    if (guidePos == 9)
+      el('bubbleArrow').style.display = 'none';
+  }
+}
+
+// load available settings from storage
+Object.entries(settings).map(s => {
+  let val = window.localStorage.getItem(s[0]);
+  val = val ? s[1].parse(val) : s[1].default;
+  updateSetting(s[0], val);
+});
+
+// load guide
+if (guidePos == -1) {
+  updateSetting('guidePos', 0);
+  window.setTimeout(() => el('mode').classList.remove('closed'), 4000);
+  window.setTimeout(setGuide, 6000);
+} else {
+  el('mode').classList.remove('closed');
+  window.setTimeout(setGuide, 1000);
+}
